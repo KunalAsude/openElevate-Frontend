ae1819647f7be2b292724529c4536af6046ad3f8
Kunal Asude <kunalasude@gmail.com>
2025-05-15 13:21:03 +0530

    Integrate Github Api

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -13,7 +13,7 @@ import { StatsCards } from "@/components/dashboard/stats-cards"
 function DashboardContent() {
   return (
     <DashboardShell>
-      <DashboardHeader heading="Dashboard" text="Welcome back! Here&rsquos an overview of your open source journey." />
+      <DashboardHeader heading="Dashboard" text="Welcome back! Here&rsquo;s an overview of your open source journey." />
 
       <StatsCards />
 
